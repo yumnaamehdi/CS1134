1966e4b56af6af10d8dae06a25b7bca83417d53d
Yumna Mehdi <78187023+yumnaamehdi@users.noreply.github.com>
2022-01-10 21:10:59 -0500

    Create Lab_1.py

---
+++ Labs/Lab_1.py
@@ -0,0 +1,22 @@
+#Number 1
+
+def add_binary(bin_num1, bin_num2):
+  """
+bin_num1 - type: str
+bin_num2 - type: str
+return value - type: str
+"""
+  
+#umber 2 
+
+def can_construct(word, letters):
+  """
+word - type: str
+letters - type: str
+return value - type: bool
+"""
+  
+#Number 3
+
+class Complex:
+  def __init(self,a,b):
